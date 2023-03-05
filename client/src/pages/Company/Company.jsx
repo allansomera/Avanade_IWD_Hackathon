46356102af00464541763bf444ea275448d4e640
Allan Somera <nagal.somera@gmail.com>
2023-03-05 03:52:35 -0800

    finished post component

--- client/src/pages/Company/Company.jsx
+++ client/src/pages/Company/Company.jsx
@@ -8,8 +8,17 @@ const Company = () => {
   return (
     <>
       <div className="company">
-        <Link to="/company/overview">Overview</Link>
-        <Link to="/company/post">Post</Link>
+        <div className="company__navbar">
+          <Link className="company__nav-item" to="/company/overview">
+            Overview
+          </Link>
+          <Link className="company__nav-item" to="/company/post">
+            Post
+          </Link>
+          <Link className="company__nav-item" to="/company/post">
+            Reviews
+          </Link>
+        </div>
 
         <Outlet />
       </div>
@@ -17,26 +26,3 @@ const Company = () => {
   );
 };
 export default Company;
-
-// <Navbar className="company__navbar">
-//   <Nav
-//     defaultActiveKey={activeLink}
-//     onSelect={(selectedKey, event) => {
-//       event.preventDefault();
-//       console.log(selectedKey);
-//       setActiveLink(selectedKey);
-//     }}
-//   >
-//     <Nav.Link
-//       as={Link}
-//       to="/company/overview"
-//       className="company__navbar-item"
-//       eventKey="overview"
-//     >
-//       Overview
-//     </Nav.Link>
-//     <Nav.Link as={Link} className="company__navbar-item" to="/company/post" eventKey="post">
-//       Post
-//     </Nav.Link>
-//   </Nav>
-// </Navbar>
