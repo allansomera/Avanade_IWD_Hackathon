11a1409b141bc8829d43785d2d6520111e167158
Allan Somera <nagal.somera@gmail.com>
2023-03-05 14:22:15 -0800

    finished company header for company component

--- client/src/pages/Company/Company.jsx
+++ client/src/pages/Company/Company.jsx
@@ -1,6 +1,6 @@
 import './Company.scss';
 import { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Link, Outlet } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Company = () => {
@@ -8,16 +8,57 @@ const Company = () => {
   return (
     <>
       <div className="company">
+        <div className="company__header">
+          <div className="company__l-header">
+            <div className="company__logo"></div>
+            <div className="company__combo-con">
+              <div className="company__name">Deloitte</div>
+              <div className="company__rating-con">
+                <div className="company__star-icon"></div>
+                <div className="company__star-rating-combo">
+                  <div className="company__star-rating-num">4.8</div>
+                  <div className="company__rating-quantity">(197)</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="company__r-header">
+            <div className="company__rh-btn-con">
+              <button className="company__rh-btn">Rate This Company</button>
+            </div>
+          </div>
+        </div>
         <div className="company__navbar">
-          <Link className="company__nav-item" to="/company/overview">
+          <NavLink
+            className="company__nav-item"
+            to="/company/overview"
+            style={({ isActive }) => ({
+              border: isActive ? '2px solid transparent' : '2px solid transparent',
+              borderBottomColor: isActive ? '#4666f1' : '#d9d9d9',
+            })}
+          >
             Overview
-          </Link>
-          <Link className="company__nav-item" to="/company/post">
+          </NavLink>
+          <NavLink
+            className="company__nav-item"
+            to="/company/post"
+            style={({ isActive }) => ({
+              border: isActive ? '2px solid transparent' : '2px solid transparent',
+              borderBottomColor: isActive ? '#4666f1' : '#d9d9d9',
+            })}
+          >
             Post
-          </Link>
-          <Link className="company__nav-item" to="/company/post">
+          </NavLink>
+          <NavLink
+            className="company__nav-item"
+            to="/company/post"
+            style={({ isActive }) => ({
+              border: isActive ? '2px solid transparent' : '2px solid transparent',
+              borderBottomColor: isActive ? '#4666f1' : '#d9d9d9',
+            })}
+          >
             Reviews
-          </Link>
+          </NavLink>
         </div>
 
         <Outlet />
