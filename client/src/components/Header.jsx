b5757a419589386f977b58212d991bfe8a19dd63
Allan Somera <nagal.somera@gmail.com>
2023-03-04 17:01:55 -0800

    finished header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,19 +5,27 @@ import './Header.scss';
 
 function Header() {
   return (
-    <header className="header">
+    <div className="header">
       <div className="header__logo-con">
         <div className="header__profile-pic"></div>
         <div className="header__username">Username</div>
       </div>
-    <input type="text" id="name" name="name" required
-       minlength="4" maxlength="8" size="10">  <div className="header__logo-search">
-    </div>
-      <div className="header__logo-right-con">
+      <div className="header__search-con">
+        <input
+          type="text"
+          className="header__search-input"
+          name="name"
+          required
+          minlength="4"
+          maxlength="8"
+          size="10"
+        />
+      </div>
+      <div className="header__right-con">
         <div className="header__notify1"></div>
         <div className="header__notify2"></div>
       </div>
-    </header>
+    </div>
   );
 }
 
