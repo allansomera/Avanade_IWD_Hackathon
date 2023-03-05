63a61b93b3958c325c66e0c54e817f4a937557c0
Allan Somera <nagal.somera@gmail.com>
2023-03-04 17:41:24 -0800

    removed uneeded variables

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import './Home.scss';
-import React, { useContext, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { UserContext } from '../../Context/UserContext';
-import axios from 'axios';
 
 function Home() {
   const [clickedItem, setClickedItem] = useState('Companies');
