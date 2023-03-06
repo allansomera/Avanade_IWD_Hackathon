63f9126b6fef213dd5b6116e6312da01db2f944c
Allan Somera <nagal.somera@gmail.com>
2023-03-05 20:50:12 -0800

    finished the review pages, added images

--- client/src/pages/ReviewProcess/step2.jsx
+++ client/src/pages/ReviewProcess/step2.jsx
@@ -1,42 +1,60 @@
 import './step2.scss';
 import { Link } from 'react-router-dom';
+import prog from '../../assets/images/ProgressIndicator_2.png';
 
 const Step2 = () => {
   return (
     <>
       <div className="step2">
+        <div className="step2__progress-con">
+          <img src={prog} />
+        </div>
         <div className="step2__heading">Job Information</div>
         <div className="step2__combo1">
           <div className="step2__job-location-con">
             <div className="step2__job-location-header"> Job Location *</div>
-            <div className="step2__job-location-input">
-              <input type="text" />
-            </div>
+            <input className="step2__job-location-input" type="text" placeholder="Job Location" />
           </div>
           <div className="step2__job-title-con">
             <div className="step2__job-title-header"> Job Title*</div>
-            <div className="step2__job-title-input">
-              <input type="text" />
-            </div>
+            <select className="step2__job-title-input" defaultValue={'DEFAULT'}>
+              <option value="DEFAULT" disabled>
+                Job Title
+              </option>
+              <option>Designer</option>
+              <option>Developer</option>
+            </select>
           </div>
         </div>
         <div className="step2__combo2">
           <div className="step2__job-type-con">
             <div className="step2__job-type-header"> Job Type*</div>
-            <div className="step2__job-type-input">
-              <input type="text" />
-            </div>
+
+            <select className="step2__job-type-input" defaultValue={'DEFAULT'}>
+              <option value="DEFAULT" disabled>
+                Job Type
+              </option>
+              <option>Designer</option>
+              <option>Developer</option>
+            </select>
           </div>
           <div className="step2__job-level-con">
             <div className="step2__job-level-header"> Job Level*</div>
-            <div className="step2__job-level-input">
-              <input type="text" />
-            </div>
+
+            <select className="step2__job-level-input" defaultValue={'DEFAULT'}>
+              <option value="DEFAULT" disabled>
+                Job Type
+              </option>
+              <option>Designer</option>
+              <option>Developer</option>
+            </select>
           </div>
         </div>
       </div>
-      <div className="step2__link">
-        <Link to="/process/rate-step3">NEXT</Link>
+      <div className="step2__next">
+        <Link className="step2__next-btn" to="/process/rate-step3">
+          NEXT
+        </Link>
       </div>
     </>
   );
--- client/src/pages/ReviewProcess/step3.jsx
+++ client/src/pages/ReviewProcess/step3.jsx
@@ -1,5 +1,7 @@
 import './step3.scss';
 import star from '../../assets/icons/star.svg';
+import { Link } from 'react-router-dom';
+import prog from '../../assets/images/ProgressIndicator_3.png';
 
 const Step3 = () => {
   let entries = [
@@ -18,19 +20,22 @@ const Step3 = () => {
   return (
     <>
       <div className="step3">
+        <div className="step3__progress-con">
+          <img src={prog} />
+        </div>
         <div className="step3__heading"> Rating </div>
         <div className="step3__entries">
           {entries.map((i) => {
             return (
               <>
-                <div className="step3__combo-con">
+                <div className="step3__combo-con" key={i}>
                   <div className="step3__c-top">
                     <div className="step3__title">{i.title}</div>
                     <div className="step3__star-con">
                       <div className="step3__star-rating">5.0</div>
                       <div className="step3__star-icon-list">
                         {[1, 2, 3, 4, 5].map((s) => {
-                          return <img className="step3__star-icon" src={star} />;
+                          return <img className="step3__star-icon" src={star} key={s} />;
                         })}
                       </div>
                     </div>
@@ -45,6 +50,9 @@ const Step3 = () => {
               </>
             );
           })}
+        </div>
+        <div className="step3__submit">
+          <Link className="step3__submit-btn">Submit</Link>
         </div>
       </div>
     </>
